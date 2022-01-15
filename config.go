109db7a60c31d9abfb662598492a0201178ccda9
Jason Chu <me@chuie.io>
2022-01-15 02:03:30 -0800

    update everything

--- config.go
+++ config.go
@@ -2,28 +2,70 @@ package lokishipper
 
 import (
 	"time"
-	"net/url"
 
-	"github.com/cortexproject/cortex/pkg/util"
+	"github.com/grafana/dskit/backoff"
+	"github.com/grafana/dskit/flagext"
 	"github.com/prometheus/common/config"
-	"github.com/prometheus/common/model"
+
+	lokiflag "github.com/grafana/loki/pkg/util/flagext"
 )
 
-// Provide the types for convenience and forward compatibility by reducing
-// direct dependencies.
-type HTTPClientConfig = config.HTTPClientConfig
-type BackoffConfig = util.BackoffConfig
+// NOTE the helm chart for promtail and fluent-bit also have defaults for these values, please update to match if you make changes here.
+const (
+	BatchWait      = 1 * time.Second
+	BatchSize  int = 1024 * 1024
+	MinBackoff     = 500 * time.Millisecond
+	MaxBackoff     = 5 * time.Minute
+	MaxRetries int = 10
+	Timeout        = 10 * time.Second
+)
 
 // Config describes configuration for a HTTP pusher client.
 type Config struct {
-	URL       *url.URL
+	URL       flagext.URLValue
 	BatchWait time.Duration
 	BatchSize int
 
-	Client HTTPClientConfig
+	Client config.HTTPClientConfig `yaml:",inline"`
 
-	BackoffConfig BackoffConfig
+	BackoffConfig backoff.Config `yaml:"backoff_config"`
 	// The labels to add to any time series or alerts when communicating with loki
-	ExternalLabels model.LabelSet
-	Timeout        time.Duration
+	ExternalLabels lokiflag.LabelSet `yaml:"external_labels,omitempty"`
+	Timeout        time.Duration     `yaml:"timeout"`
+
+	// The tenant ID to use when pushing logs to Loki (empty string means
+	// single tenant mode)
+	TenantID string `yaml:"tenant_id"`
+
+	StreamLagLabels flagext.StringSliceCSV `yaml:"stream_lag_labels"`
+}
+
+// UnmarshalYAML implement Yaml Unmarshaler
+func (c *Config) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	type raw Config
+	var cfg raw
+	if c.URL.URL != nil {
+		// we used flags to set that value, which already has sane default.
+		cfg = raw(*c)
+	} else {
+		// force sane defaults.
+		cfg = raw{
+			BackoffConfig: backoff.Config{
+				MaxBackoff: MaxBackoff,
+				MaxRetries: MaxRetries,
+				MinBackoff: MinBackoff,
+			},
+			BatchSize:       BatchSize,
+			BatchWait:       BatchWait,
+			Timeout:         Timeout,
+			StreamLagLabels: []string{"filename"},
+		}
+	}
+
+	if err := unmarshal(&cfg); err != nil {
+		return err
+	}
+
+	*c = Config(cfg)
+	return nil
 }
--- multi.go
+++ multi.go
@@ -1,91 +1,74 @@
 package lokishipper
 
 import (
-	"bytes"
 	"errors"
-	"fmt"
-	"time"
+	"sync"
 
-	"github.com/go-kit/kit/log"
-	"github.com/prometheus/common/model"
+	"github.com/go-kit/log"
+	"github.com/prometheus/client_golang/prometheus"
+
+	"github.com/grafana/loki/clients/pkg/promtail/api"
 )
 
-// The MultiError type implements the error interface, and contains the
-// Errors used to construct it.
-type MultiError []error
-
-// Returns a concatenated string of the contained errors
-func (es MultiError) Error() string {
-	var buf bytes.Buffer
-
-	if len(es) > 1 {
-		_, _ = fmt.Fprintf(&buf, "%d errors: ", len(es))
-	}
-
-	for i, err := range es {
-		if i != 0 {
-			buf.WriteString("; ")
-		}
-		buf.WriteString(err.Error())
-	}
-
-	return buf.String()
-}
-
-// Add adds the error to the error list if it is not nil.
-func (es *MultiError) Add(err error) {
-	if err == nil {
-		return
-	}
-	if merr, ok := err.(MultiError); ok {
-		*es = append(*es, merr...)
-	} else {
-		*es = append(*es, err)
-	}
-}
-
-// Err returns the error list as an error or nil if it is empty.
-func (es MultiError) Err() error {
-	if len(es) == 0 {
-		return nil
-	}
-	return es
-}
-
 // MultiClient is client pushing to one or more loki instances.
-type MultiClient []Client
+type MultiClient struct {
+	clients []Client
+	entries chan api.Entry
+	wg      sync.WaitGroup
+
+	once sync.Once
+}
 
 // NewMulti creates a new client
-func NewMulti(logger log.Logger, cfgs ...Config) (Client, error) {
+func NewMulti(reg prometheus.Registerer, logger log.Logger, cfgs ...Config) (Client, error) {
 	if len(cfgs) == 0 {
 		return nil, errors.New("at least one client config should be provided")
 	}
 
 	clients := make([]Client, 0, len(cfgs))
 	for _, cfg := range cfgs {
-		client, err := New(cfg, logger)
+		client, err := New(reg, cfg, logger)
 		if err != nil {
 			return nil, err
 		}
 		clients = append(clients, client)
 	}
-	return MultiClient(clients), nil
+	multi := &MultiClient{
+		clients: clients,
+		entries: make(chan api.Entry),
+	}
+	multi.start()
+	return multi, nil
 }
 
-// Handle Implements api.EntryHandler
-func (m MultiClient) Handle(labels model.LabelSet, time time.Time, entry string) error {
-	var result MultiError
-	for _, client := range m {
-		if err := client.Handle(labels, time, entry); err != nil {
-			result.Add(err)
+func (m *MultiClient) start() {
+	m.wg.Add(1)
+	go func() {
+		defer m.wg.Done()
+		for e := range m.entries {
+			for _, c := range m.clients {
+				c.Chan() <- e
+			}
 		}
-	}
-	return result.Err()
+	}()
+}
+
+func (m *MultiClient) Chan() chan<- api.Entry {
+	return m.entries
 }
 
 // Stop implements Client
-func (m MultiClient) Stop() {
-	for _, c := range m {
+func (m *MultiClient) Stop() {
+	m.once.Do(func() { close(m.entries) })
+	m.wg.Wait()
+	for _, c := range m.clients {
 		c.Stop()
+	}
+}
+
+// StopNow implements Client
+func (m *MultiClient) StopNow() {
+	for _, c := range m.clients {
+		c.StopNow()
 	}
 }
